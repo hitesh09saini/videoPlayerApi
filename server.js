8e5f29df9cb5fac28065b1f56eb79f4861447f0a
Hitesh Saini <hitesh9694saini@gmail.com>
2024-01-12 19:33:38 +0530

    p

--- server.js
+++ server.js
@@ -1,40 +1,42 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const videoRoutes = require('./routes/videoRoutes');
 const subtitleRoutes = require('./routes/subtitleRoutes');
 
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
 const clientURL = process.env.CLIENT_URL;
 
-app.use(cors({
-  origin: clientURL,
-}));
 
 app.use(express.json()); // Parse incoming JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse incoming form data
+
+app.use('/videos', videoRoutes);
+app.use('/subtitles', subtitleRoutes);
+
+app.use(cors({
+  origin: clientURL,
+}));
 
 app.get('/', (req, res) => {
   res.json({ success: true, message: "Hello server is running!" });
 });
 
+
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('MongoDB is connected');
 }).catch(err => {
-  console.log('Mongo Error: ' + err);
-})
+  console.error('MongoDB connection error:', err);
+  process.exit(1);
+});
 
-
-app.use('/videos', videoRoutes);
-app.use('/subtitles', subtitleRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
