421997018f328dbf7398d6328cf100b35285e976
Hitesh Saini <hitesh9694saini@gmail.com>
2024-01-12 17:56:32 +0530

    ch

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+const clientURL = process.env.CLIENT_URL;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: clientURL,
+}));
+
+app.use(express.json()); // Parse incoming JSON requests
+app.use(express.urlencoded({ extended: true })); // Parse incoming form data
 
 app.get('/', (req, res) => {
   res.json({ success: true, message: "Hello server is running!" });
