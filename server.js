21989e63fde566d4024cd7262c373b1209d4341d
Hitesh Saini <hitesh9694saini@gmail.com>
2024-01-12 19:05:15 +0530

    p

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.get('/', (req, res) => {
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useCreateIndex: true
 }).then(() => {
   console.log('MongoDB is connected');
 }).catch(err => {
