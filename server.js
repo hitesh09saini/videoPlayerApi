7f1252d6c0b3bc05b571d5046d82022f577ee3bd
Hitesh Saini <hitesh9694saini@gmail.com>
2024-01-13 22:30:38 +0530

    push

--- server.js
+++ server.js
@@ -10,37 +10,38 @@ const subtitleRoutes = require('./routes/subtitleRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 const clientURL = process.env.CLIENT_URL;
 
+console.log(clientURL);
 
-app.use(express.json()); // Parse incoming JSON requests
-app.use(express.urlencoded({ extended: true })); // Parse incoming form data
+// CORS middleware setup
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/videos', videoRoutes);
 app.use('/subtitles', subtitleRoutes);
 
-app.use(cors({
-  origin: '*'||clientURL,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
-}));
-
 app.get('/', (req, res) => {
-  res.json({ success: true, message: "Hello server is running!" });
+  res.json({ success: true, message: 'Hello, the server is running!' });
 });
-
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log('MongoDB is connected');
-}).catch(err => {
-  console.error('MongoDB connection error:', err);
-  process.exit(1);
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB is connected');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
